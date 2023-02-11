33fe3f1e24446df46d448f3db5d0be528edde564
zhifengzhang60@hotmail.com <zhifengzhang60@hotmail.com>
2023-02-12 01:56:26 +0800

    ...

--- src/python/Contexts/Functor.py
+++ src/python/Contexts/Functor.py
@@ -1,19 +1,22 @@
-from typing import TypeVar, TypeVarTuple, Generic, Callable
+from __future__ import annotations
+from typing import TypeVar, Generic, Callable
+import abc
+from functools import partial
 
-A=TypeVar('A')
-As=TypeVarTuple('As')
-B=TypeVar('B')
-Bs=TypeVarTuple('Bs')
+A = TypeVar('A')
+B = TypeVar('B')
 
-class Functor(Generic[A]):
-    pass
+class Functor(Generic[A],metaclass=abc.ABCMeta):
+    def const(a,b):
+        return a
+    @abc.abstractmethod
+    def fmap(self,f:Callable[[A],B])->Functor[B]:
+        ...
+    def mrb(self,a): return self.fmap(partial(Functor.const,a))
 
 class ListFunctor(list[A],Functor[A]):
-
-    def fmap(self,f:Callable[[A],B])->Functor[B]:
-        return ListFunctor(map(f,self))
+    #def fmap(self,f:Callable[[A],B])->Functor[B]: return ListFunctor(map(f,self))
+    def fmap(self,f:Callable[[A],B])->Functor[B]: return ListFunctor((f(x) for x in self))
     
 class TupleFunctor(tuple[A,...],Functor[A]):
-
-    def fmap(self,f:Callable[[A],B])->Functor[B]:
-        return TupleFunctor(map(f,self))
+    def fmap(self,f:Callable[[A],B])->Functor[B]: return TupleFunctor(map(f,self))
