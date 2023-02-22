21c879142107c8c59fc10ef925149e203b073d74
zhifengzhang60@hotmail.com <zhifengzhang60@hotmail.com>
2023-02-22 23:27:01 +0800

    added the leaf data type for binary tree

--- src/python/Signal/btnt.py
+++ src/python/Signal/btnt.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
-from typing import TypeVar, Callable
+from typing import TypeVar, Callable, TypeAlias, Type
 
-C = TypeVar("C")
-T = TypeVar("T")
+A = TypeVar("A")
+B = TypeVar("B")
 
 # 二叉树的节点
 
-
-class btn:
+LeafType:TypeAlias = None # for python 3.12 we can use `type Leaf=None`
+Leaf = None # Leaf object
+class btn():
     """
     Binary tree node
 
@@ -18,17 +19,16 @@ class btn:
     right: right child node, btn object
     tag: tag of current node, Any
     """
+    def __init__(self, content:A,left:btn|LeafType,right:btn|LeafType,tag:B=None):
+        self.content:A|None = content
+        self.left:btn|LeafType = left
+        self.right:btn|LeafType = right
+        self.tag:B|None = tag
 
-    def __init__(self, content:C,left:btn,right:btn,tag:T=None):
-        self.content = content
-        self.left = left
-        self.right = right
-        self.tag = tag
+Node:TypeAlias = btn|LeafType
 
 # 产生`level`层的二叉树
-
-
-def makebtnt(level:int,content:C,lp:Callable[[C],C],rp:Callable[[C],C]) -> btn:
+def makebtnt(level:int,content:A,lp:Callable[[A],A],rp:Callable[[A],A]) -> Node:
     """
     Generate btn tree
 
@@ -45,7 +45,7 @@ def makebtnt(level:int,content:C,lp:Callable[[C],C],rp:Callable[[C],C]) -> btn:
     """
     if not isinstance(level, int) or level < 0:
         raise ValueError('invalid input in makebtnt')
-    root = btn(content, None, None, None)  # type: ignore
+    root = btn(content, Leaf, Leaf, None) 
     if level > 0:
         if lp != None:
             root.left = makebtnt(level-1, lp(content), lp, rp)
@@ -58,38 +58,34 @@ def makebtnt(level:int,content:C,lp:Callable[[C],C],rp:Callable[[C],C]) -> btn:
     return root
 
 # 提取`(level,block)`节点
-
-
 def btnt2btn(root, level, block):
-    if level == 0 or root == None:
+    if level == 0 or root == Leaf:
         node = root
     else:
         if (block % 2) == 0:
-            node = btnt2btn(root.left, level-1, block/2)
+            node = btnt2btn(root.left, level-1, block>>1)
         else:
-            node = btnt2btn(root.right, level-1, (block-1)/2)
+            node = btnt2btn(root.right, level-1, (block-1)>>1)
     return node
 
 # 提取以节点`(level,block)`为根的子树
-
-
-def btn2branch(self, level:int, block:int)->btn:
+def btn2branch(self, level:int, block:int)->Node:
     if level > 0:
         if (block % 2) == 0:
-            if self.left == None:
-                self.left = btn(None, None, None, None)  # type: ignore
-            self = btn2branch(self.left, level-1, block/2)
+            if self.left == Leaf:
+                self.left = btn(None, Leaf, Leaf, None)
+            self = btn2branch(self.left, level-1, block>>1)
         else:
-            if self.right == None:
-                self.right = btn(None, None, None, None)  # type: ignore
-            self = btn2branch(self.right, level-1, int((block-1)/2))
+            if self.right == Leaf:
+                self.right = btn(None, Leaf, Leaf, None)
+            self = btn2branch(self.right, level-1, (block-1)>>1)
     return self
 
 
-def height(node: btn):
-    if (node == None):
+def height(node: Node)->int:
+    if (node == Leaf):
         return 0
-    else:
+    elif (node!=None):
         # compute the height of each subtree
         lheight = height(node.left)
         rheight = height(node.right)
@@ -99,11 +95,10 @@ def height(node: btn):
             return (lheight + 1)
         else:
             return (rheight + 1)
+    else: return 0
 
 # Function to  print level order traversal of tree
-
-
-def print_level_order(root: btn):
+def print_level_order(root: Node):
     h = height(root)
     for i in range(1, h+1):
         print_current_level(root, i)
@@ -111,7 +106,7 @@ def print_level_order(root: btn):
 
 
 # Print nodes at a current level
-def print_current_level(root: btn, level: int):
+def print_current_level(root: Node, level: int):
     if root is None:
         return
     if level == 1:
